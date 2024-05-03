39c89d70a438fb33e89b881378314fbe90df6aa1
Kev R <48894729+kevklash@users.noreply.github.com>
2024-05-03 00:19:30 -0600

    Completed part 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber;
+        // Ask to generate or to input a magic number
+        Console.Write("Do you want to input a magic number? (y/n) If no, a random magic number will be generated. ");
+        string response = Console.ReadLine();
+        if (response == "y")
+        {
+            Console.Write("What is the magic number? ");
+            magicNumber = int.Parse(Console.ReadLine());
+        }
+        else
+        {
+            Random randomGenerator = new Random();
+            magicNumber = randomGenerator.Next(1, 101);
+        }
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } 
     }
 }
